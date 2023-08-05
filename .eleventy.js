212a039f6842167f86e4d4c7363bc7f80e0328c4
min <liwoyadans@gmail.com>
2023-08-05 15:21:57 -0700

    Fixing passthroughcopy path for webfonts

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/img");
-    eleventyConfig.addPassthroughCopy("src/fonts");
+    eleventyConfig.addPassthroughCopy("src/webfonts");
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/favicon.ico");
 
