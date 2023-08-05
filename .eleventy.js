b090228ec0b616bcb28282fadf67c77ed1253f57
min <liwoyadans@gmail.com>
2023-08-05 14:53:22 -0700

    Building layout a little more, installing and adding SCSS compilation Also adding a resource shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,41 @@
+const sass = require("sass");
+const path = require("path");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/img");
     eleventyConfig.addPassthroughCopy("src/fonts");
     eleventyConfig.addPassthroughCopy("src/js");
+
+    // Compile Sass to CSS
+    eleventyConfig.addTemplateFormats("scss");
+    eleventyConfig.addExtension("scss", {
+        outputFileExtension: "css",
+
+        compile(content, inputPath) {
+            let parsed = path.parse(inputPath)
+            if(parsed.name.startsWith("_")) {
+                return;
+            }
+
+            console.log('Compiling scss...', inputPath)
+
+            return (data) => {
+                let result = sass.compile(inputPath)
+                return result.css
+            }
+        }
+    });
+
+    // Resources Shortcode
+    eleventyConfig.addPairedShortcode("resource", function(content, title, url) {
+        var openingDiv = '<div class="resource">'
+        var link = '<div class="resource-title"><a href="' + url + '">' + title + '</a></div>'
+        var contentDiv = '<div class="resource-content ps-4">' + content + '</div>'
+        var closingDiv = '</div>'
+
+        return openingDiv + link + contentDiv + closingDiv;
+    });
 
     return {
         passthroughFileCopy: true,
